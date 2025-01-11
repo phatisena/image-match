16b4de8a5695a73d493df01e3fc58048f8167923
Phatisena <naphatism@hotmail.com>
2025-01-11 11:46:30 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,19 +12,22 @@ namespace images {
     export function find(inimg:Image,arrimg:Image[],percent:number=50) {
         let nv = 0,anv = 0
         let uimg: Image = null
+        percent = ((percent - 1) % 100) + 1
         while (percent > 0) {
             if (percent <= 0) break;
-        for (let i = 0;i < arrimg.length;i++) {
-            nv = 0
-            uimg = arrimg[i]
-            if (uimg.width != inimg.width || uimg.height != inimg.height) return -1;
-            for (let x = 0;x < inimg.width;x++) {
-                for (let y = 0;y < inimg.height;y++) {
-                    if (uimg.getPixel(x,y) == inimg.getPixel(x,y)) {nv += 1}
+            for (let i = 0;i < arrimg.length;i++) {
+                nv = 0
+                uimg = arrimg[i]
+                if (uimg.width != inimg.width || uimg.height != inimg.height) return -1;
+                for (let x = 0;x < inimg.width;x++) {
+                    for (let y = 0;y < inimg.height;y++) {
+                        if (uimg.getPixel(x,y) == inimg.getPixel(x,y)) {nv += 1}
+                    }
                 }
+                anv = Math.map(nv, 0, (inimg.width*inimg.height), 0, 100)
+                if (anv > percent) return i;
             }
-            anv = Math.map(nv, 0, (inimg.width*inimg.height), 0, 100)
-            if (anv > percent) return i;
+            percent -= 1
         }
         percent -= 1
         }
